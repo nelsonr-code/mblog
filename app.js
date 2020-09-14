61c3d039c7ffa98ac18b6e320e947e2afa475a7b
Carlos <carlos.gonzalez@sitiouno.com>
2020-09-14 18:09:21 -0400

    test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
-// coonsole.log('variables de entorno!', process.env);
+coonsole.log('variables de entorno!', process.env);
 
 import server from './src/infraestructure/express/server';
 import mongooseConnectionHandler  from './src/infraestructure/mongoose/connection';
